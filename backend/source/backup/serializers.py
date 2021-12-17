8576c9ecdc8899db18f1811157f908e79f9cdb16
John Charlton <coolmule0@hotmail.co.uk>
2021-12-17 11:27:11 +0000

    Add 'key' field to sources

--- backend/source/backup/serializers.py
+++ backend/source/backup/serializers.py
@@ -8,8 +8,11 @@ from .models.rsync import RsyncSource, RsyncDestination
 from .models.trakt import TraktSource
 from .models.twitter import TwitterSource
 
+class BaseSourceSerializer(serializers.HyperlinkedModelSerializer):
+    key = serializers.CharField()
 
-class RsyncSourceSerializer(serializers.HyperlinkedModelSerializer):
+
+class RsyncSourceSerializer(BaseSourceSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
     class Meta:
@@ -17,7 +20,7 @@ class RsyncSourceSerializer(serializers.HyperlinkedModelSerializer):
         fields = '__all__'
 
 
-class RsyncDestinationSerializer(serializers.HyperlinkedModelSerializer):
+class RsyncDestinationSerializer(BaseSourceSerializer):
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
     class Meta:
@@ -25,31 +28,31 @@ class RsyncDestinationSerializer(serializers.HyperlinkedModelSerializer):
         fields = '__all__'
 
 
-class TwitterSourceSerializer(serializers.HyperlinkedModelSerializer):
+class TwitterSourceSerializer(BaseSourceSerializer):
     class Meta:
         model = TwitterSource
         fields = '__all__'
 
 
-class RedditSourceSerializer(serializers.HyperlinkedModelSerializer):
+class RedditSourceSerializer(BaseSourceSerializer):
     class Meta:
         model = RedditSource
         fields = '__all__'
 
 
-class HackerNewsSourceSerializer(serializers.HyperlinkedModelSerializer):
+class HackerNewsSourceSerializer(BaseSourceSerializer):
     class Meta:
         model = HackerNewsSource
         fields = '__all__'
 
 
-class RssSourceSerializer(serializers.HyperlinkedModelSerializer):
+class RssSourceSerializer(BaseSourceSerializer):
     class Meta:
         model = RssSource
         fields = '__all__'
 
 
-class FileSystemSourceSerializer(serializers.HyperlinkedModelSerializer):
+class FileSystemSourceSerializer(BaseSourceSerializer):
     class Meta:
         model = FileSystemSource
         fields = '__all__'
